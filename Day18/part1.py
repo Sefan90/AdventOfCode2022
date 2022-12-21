0f830a395129eebd29b6694ec0c140618127641b
Stefan <stevetv90@gmail.com>
2022-12-21 21:25:50 +0100

    Day 18 Part 1

---
+++ Day18/part1.py
@@ -0,0 +1,36 @@
+def part1():
+    file = [[int(c) for c in r.replace("\n","").split(",")] for r in open('input.txt','r')]
+    size = [0,0,0]
+    for r in file:
+        for i in range(len(r)):
+            if r[i]+1 > size[i]:
+                size[i] = r[i]+1
+    print(file)
+    print(size)
+    map3d = []
+    for i in range(size[0]):
+        map3d.append([])
+        for j in range(size[1]):
+            map3d[i].append([])
+            for k in range(size[2]):
+                map3d[i][j].append([])
+                if [i,j,k] in file:
+                    map3d[i][j][k] = 1
+                else:
+                    map3d[i][j][k] = 0
+
+    count = 0
+    for i in range(size[0]):
+        for j in range(size[1]):
+            for k in range(size[2]):
+                if map3d[i][j][k] == 1:
+                    tmp_count = 6
+                    for l in [[1,0,0],[-1,0,0],[0,1,0],[0,-1,0],[0,0,1],[0,0,-1]]:
+                        if (i+l[0]) < size[0] and (j+l[1]) < size[1] and (k+l[2]) < size[2]:
+                            if map3d[i+l[0]][j+l[1]][k+l[2]] == 1:
+                                tmp_count -= 1
+                    print(tmp_count)
+                    count += tmp_count
+    print(count)
+    
+part1()
